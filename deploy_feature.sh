b3643a7b7cd9ff8455fb72814ac1ff6dcb8fbecc
cmnkumar <nkumar.cm@gmail.com>
2017-08-21 15:01:44 +0530

    Create deploy_feature.sh

---
+++ deploy_feature.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+	echo "inside deploy shell script"
+  
+AWS_ACCESS_KEY_ID=$(eval "echo $$AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(eval "echo $$AWS_SECRET_ACCESS_KEY")
+
+
+
+configure_aws_cli() {
+	aws --version
+	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+	#aws configure set default.region $AWS_REGION
+	aws configure set default.output json
+	echo "Configured AWS CLI."
+}
+configure_aws_cli
