2512e2638b970a0601d4190c19002fa59300d5bb
cmnkumar <nkumar.cm@gmail.com>
2017-08-21 15:11:20 +0530

    Update deploy_feature.sh

--- deploy_feature.sh
+++ deploy_feature.sh
@@ -12,6 +12,7 @@ configure_aws_cli() {
 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 	#aws configure set default.region $AWS_REGION
 	aws configure set default.output json
+	aws configure
 	echo "Configured AWS CLI."
 }
 configure_aws_cli
