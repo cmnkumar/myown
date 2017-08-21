9f19d22da0a7ac2063e46ce0f9189973da054257
cmnkumar <nkumar.cm@gmail.com>
2017-08-21 17:18:17 +0530

    Update deploy_feature.sh

--- deploy_feature.sh
+++ deploy_feature.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 	echo "inside deploy shell script"
-  
-AWS_ACCESS_KEY_ID=$(eval "echo $$AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=$(eval "echo $$AWS_SECRET_ACCESS_KEY")
+ 
+aws configure get aws_access_key_id
+
+echo "Aws access key "+ $$AWS_ACCESS_KEY_ID
+echo "Aws secret key "+ $$AWS_SECRET_ACCESS_KEY
 
 
 
