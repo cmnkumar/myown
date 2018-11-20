ba97bd3d5579400dd464fd625cb904d5d414df4e
cmnkumar <nkumar.cm@gmail.com>
2018-11-20 23:17:41 +0530

    Update deploy_feature.sh

--- deploy_feature.sh
+++ deploy_feature.sh
@@ -2,7 +2,7 @@
 	echo "testing in23 blah lah inside deploy shell script  v "
 	
 configure_aws_cli() {
-	aws --version
+	aws --version 
 	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 	echo $AWS_ACCESS_KEY_ID
