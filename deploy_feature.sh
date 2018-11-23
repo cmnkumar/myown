58b4bd9111904ab4a47e44fb5c3727b7d0f3edad
cmnkumar <nkumar.cm@gmail.com>
2018-11-23 09:37:45 +0530

    Update deploy_feature.sh

--- deploy_feature.sh
+++ deploy_feature.sh
@@ -6,7 +6,7 @@ configure_aws_cli() {
 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 	echo $AWS_ACCESS_KEY_ID
 	echo $AWS_SECRET_ACCESS_KEY
-	AWS_ACCOUNT_ID=123456789090
+	AWS_ACCOUNT_ID=123456789099
 	
 	#aws configure set default.region $AWS_REGION
 	aws configure set default.output json
